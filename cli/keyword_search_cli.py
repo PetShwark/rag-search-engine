fb2950500bb2a031b8c15cad2a2ca13143911e26
Peter Schwenk <paschwenk@gmail.com>
2026-08-17 13:40:58 -0400

    After "07-Tokenizeation" lesson

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -2,6 +2,38 @@ import argparse
 import json
 import string
 
+
+def token_found(search_for: list[str], search_in: list[str]) -> bool:
+    """
+    Looks for the strings in the search_for list in the search_in list of strings.
+    It is assumed that the strings have been tokenized (lowercase-d and punctuation
+    removed).
+    """
+    for search_in_token in search_in:
+        for search_for_token in search_for:
+            if search_for_token in search_in_token:
+                return True
+    return False
+
+
+def depunctuate(input: str) -> str:
+    punctranslator = str.maketrans("", "", string.punctuation)
+    return input.translate(punctranslator)
+
+
+def tokenize(input: str) -> list[str]:
+    """
+    Takes a string and returns tokens.  The tokens are all in lowercase and
+    devoid of punctuation.
+    """
+    result: list[str] = []
+    words = input.split()
+    for word in words:
+        if word:
+            result.append(depunctuate(word).lower().strip())
+    return result
+
+
 def search_movies_from_json_file(json_file: str, search_for: str, max_results: int = 5) -> list[str]:
     result: list[str] = []
     with open(json_file, "r") as file:
@@ -10,16 +42,16 @@ def search_movies_from_json_file(json_file: str, search_for: str, max_results: i
         except json.JSONDecodeError, UnicodeDecodeError:
             print(f"Error decoding JSON file.")
     result_count = 0
-    punctranslator = str.maketrans("", "", string.punctuation)
-    search_for = search_for.translate(punctranslator).lower()
-    for movie in movie_data["movies"]:
-        if result_count >= max_results:
-            break
-        if movie.get("title"):
-            movie_title_to_search = movie["title"].translate(punctranslator).lower()
-            if search_for in movie_title_to_search:
-                result.append(movie["title"])
-                result_count += 1
+    search_for_tokens = tokenize(search_for)
+    if movie_data.get("movies"):
+        for movie in movie_data["movies"]:
+            if result_count >= max_results:
+                break
+            if movie.get("title"):
+                movie_title_tokens = tokenize(movie["title"])
+                if token_found(search_for_tokens, movie_title_tokens):
+                    result.append(movie["title"])
+                    result_count += 1
     return result
             
 
